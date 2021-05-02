172ec14eb6b51c43a0b3f5bb93a0fff8a54bb68d
David <Davidgarrosa_7@hotmail.com>
2021-05-02 20:30:39 +0200

    capsulados los elementos con los que voy a trabajar

---
+++ JS/main.js
@@ -0,0 +1,5 @@
+const listaTareas = new Array();
+let btnGuardar = document.querySelector('#guardar');
+let inputTitulo = document.querySelector('#titulo');
+let selectPrioridad = document.querySelector('#prioridad');
+let ulTareas = document.querySelector('#tareas');
